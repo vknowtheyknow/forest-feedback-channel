6befb230536656ba6bc4b3bac80ab72c1f46f078
Patchaa_GIT <56762841+vknowtheyknow@users.noreply.github.com>
2024-04-04 22:08:48 +0700

    Update merge.py

--- lora_receiver/merge.py
+++ lora_receiver/merge.py
@@ -7,6 +7,7 @@ sys.path.append('..')
 
 import config
 import uploader
+import __main__
 
 CROP_SIZE = config.CROP_SIZE
 WIDTH = config.LORA_IMG_WIDTH
@@ -15,6 +16,7 @@ HEIGHT = config.LORA_IMG_HEIGHT
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 RX_BUFFER = './lora_receiver/rx_buffer/'
+MISS_IMG = './lora_receiver/miss_img/'
 
 def is_update(path):
     f = open(f'{path}/update.txt', 'r')
@@ -41,6 +43,7 @@ def segment_merge(path):
 
 count = 0
 img_list = os.listdir(RX_BUFFER)
+fail_list = []
 for img in img_list:
     if not os.path.isdir(RX_BUFFER + img) or img == 'sp' or img == 'uploaded':
         continue
@@ -63,12 +66,43 @@ for img in img_list:
             f.write(segment_merge(f'{RX_BUFFER}{img}/{part}'))
             f.close()
         x,y = part.split('_')
-        try:
-            result_img.paste(Image.open(f'{RX_BUFFER}{img}/{part}.jpg'), (CROP_SIZE*int(x), CROP_SIZE*int(y)))
-            print(f'{part} ok')
-        except:
+        #add
+        part_parts = sorted(int(i) for i in os.listdir(RX_BUFFER + img +'/'+ part))
+        #print(part_parts,x,y)
+        #print(f'{len(part_parts)} != {int(part_parts[-1]) + 1}')
+        if len(part_parts) != int(part_parts[-1]) + 1:
+            fail_list.append([int(x),int(y)])
             print(f'{part} fail')
+        else:
+            try:
+                result_img.paste(Image.open(f'{RX_BUFFER}{img}/{part}.jpg'), (CROP_SIZE*int(x), CROP_SIZE*int(y)))
+                print(f'{part} ok')
+            except:
+                print(f'{part} fail')
+                
+                fail_list.append([int(x),int(y)])
+    fail_list.insert(0,img)
+    print(f'send again')
+    print(fail_list[1:])
+    fimg = int(img)
+    header = b''
+    header = bytes([fimg//256, fimg%256])
+    for i in fail_list[1:]:
+        x,y = i
+        header += bytes([x,y])
+    print(header)
+ 
+    if len(fail_list) > 1:
+        if not os.path.isdir(f'{MISS_IMG}{img}'):
+            os.mkdir(f'{MISS_IMG}{img}')
+        f = open(f'{MISS_IMG}{img}/missing.txt', 'wb')
+        f.write(header)
+        f.close
+    fail_list=[]
+    #loras[0].lora_tx(header,4,0)
+    
     result_img.save(RX_BUFFER + img + '.jpg')
+    
     try:
         print(uploader.upload_image(RX_BUFFER + img + '.jpg'))
         uploader.upload_tx_status()
@@ -77,3 +111,4 @@ for img in img_list:
         print('User exit (Ctrl + C)')
     except:
         print('Post error')
+    
